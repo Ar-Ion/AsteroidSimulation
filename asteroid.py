05c423da17e488303514115a7d4b67f107c2cc5b
Arion Zimmermann <arionz@caltech.edu>
2024-03-19 17:40:40 -0700

    Isolate asteroid primitive for better modularity

---
+++ asteroid.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+import omni
+from omni.isaac.core.prims import RigidPrim
+
+from dynamics import DynamicObject
+
+class Asteroid(DynamicObject):
+    def __init__(self, prim_path):
+
+        DynamicObject.__init__(self, prim_path)
+
+        self._prim = RigidPrim(
+            prim_path=prim_path, 
+            name="asteroid",
+            position=np.array([0.0, 0.0, 0.0]),
+            orientation=np.array([1.0, 0.0, 0.0, 0.0]),
+            scale=[1.0, 1.0, 1.0]
+        )
+
+        # Disable motion damping for the asteroid
+        stage = omni.usd.get_context().get_stage()
+        prim_ref = stage.GetPrimAtPath(prim_path)
+
+        omni.kit.commands.execute("AddPhysicsComponentCommand", usd_prim=prim_ref, component="PhysicsRigidBodyAPI")
+        omni.kit.commands.execute("AddPhysicsComponentCommand", usd_prim=prim_ref, component="PhysicsMassAPI")
+
+        damping_attr = prim_ref.GetAttribute("physxRigidBody:angularDamping")
+        damping_attr.Set(0.0)
+
+        self._center_attr = prim_ref.GetAttribute("physics:centerOfMass")
+        self._inertia_attr = prim_ref.GetAttribute("physics:diagonalInertia")
+        self._axes_attr = prim_ref.GetAttribute("physics:principalAxes")
+
+    def get_prim(self):
+        return self._prim
+    
+    def setup(self):
+        velocities = np.full((1, 3), fill_value=0.2)
+        self._prim.set_angular_velocity(velocities)
+        self.start_tf_publisher()
--- space_environment.py
+++ space_environment.py
@@ -1,17 +1,13 @@
-from environment_base import Environment
-
-import omni
-import omni.usd
-from omni.isaac.core import World, PhysicsContext
-from omni.isaac.core.prims import RigidPrim
-from omni.isaac.core.utils.stage import add_reference_to_stage
-from omni.isaac.core.utils.nucleus import get_assets_root_path
-import omni.isaac.core.utils.prims as prim_utils
-
-from pxr import Gf, Sdf, UsdPhysics
 import carb
 import numpy as np
 
+import omni.usd
+from omni.isaac.core import PhysicsContext
+from omni.isaac.core.utils.stage import add_reference_to_stage
+import omni.isaac.core.utils.prims as prim_utils
+
+from environment_base import Environment
+from asteroid import Asteroid
 
 class SpaceEnvironment(Environment):
     def __init__(self, world):
@@ -24,30 +20,9 @@ class SpaceEnvironment(Environment):
             carb.log_error("Failed to load asteroid USD model")
         
         # Load USD to rigid body primitive
-        add_reference_to_stage(usd_path=usd_path, prim_path="/World/Asteroid")
-        
-        self._asteroid_prim = RigidPrim(
-            prim_path="/World/Asteroid", 
-            name="asteroid",
-            position=np.array([0.0, 0.0, 0.0]),
-            orientation=np.array([1.0, 0.0, 0.0, 0.0]),
-            scale=[1.0, 1.0, 1.0]
-        )
-
-        # Disable motion damping for the asteroid
-        stage = omni.usd.get_context().get_stage()
-        prim_ref = stage.GetPrimAtPath("/World/Asteroid")
-
-        omni.kit.commands.execute("AddPhysicsComponentCommand", usd_prim=prim_ref, component="PhysicsRigidBodyAPI")
-        omni.kit.commands.execute("AddPhysicsComponentCommand", usd_prim=prim_ref, component="PhysicsMassAPI")
-
-        damping_attr = prim_ref.GetAttribute("physxRigidBody:angularDamping")
-        damping_attr.Set(0.0)
-
-        self._asteroid_center_attr = prim_ref.GetAttribute("physics:centerOfMass")
-        self._asteroid_inertia_attr = prim_ref.GetAttribute("physics:diagonalInertia")
-        self._asteroid_axes_attr = prim_ref.GetAttribute("physics:principalAxes")
-
+        asteroid_prim_path = "/World/asteroid"
+        add_reference_to_stage(usd_path=usd_path, prim_path=asteroid_prim_path)
+        self._asteroid = Asteroid(asteroid_prim_path)
 
         # Disable gravity
         self._physics_prim = PhysicsContext(prim_path="/World/physicsContext")
@@ -55,7 +30,7 @@ class SpaceEnvironment(Environment):
 
         # Setup lighting
         self._sun_prim = prim_utils.create_prim(
-            "/World/Sun",
+            "/World/sun",
             "DistantLight",
             position=np.array([0.0, 0.0, 0.0]),
             orientation=np.array([1.0, 0.0, 0.0, 0.0]),
@@ -66,12 +41,10 @@ class SpaceEnvironment(Environment):
         )
 
         # Add elements to scene
-        self._world.scene.add(self._asteroid_prim)
-
+        self._world.scene.add(self._asteroid.get_prim())
 
     def setup_post_load(self):
-        velocities = np.full((1, 3), fill_value=0.2)
-        self._asteroid_prim.set_angular_velocity(velocities)
+        self._asteroid.setup()
 
     def setup_pre_reset(self):
         return
