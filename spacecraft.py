b40252dd8b5ea53aacfd88ba8453e754a7178c6d
Arion Zimmermann <arionz@caltech.edu>
2024-03-19 17:43:29 -0700

    Change camera name on main spacecraft

--- spacecraft.py
+++ spacecraft.py
@@ -4,7 +4,7 @@ from Sensors.camera import ROSCamera
 class Spacecraft(Robot):
     def setup_robot(self):
         self.add_sensor(ROSCamera(
-            name="cam1",
+            name="camera",
             resolution=(1024, 1024),
             fps=60
         ))
